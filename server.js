ebcfd06e5dd5a7e39e668dcc96039dd23d5df859
alchemist <159976232+full-metal-alchemist-dev@users.noreply.github.com>
2024-07-19 20:19:07 -0600

    config
    configuracion basica

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+app.set('port', process.env.PORT || 9000)
+
+// routes -------------------------------------------
+app.get('/',(req,res)=>{
+    res.send('Bienvenido a mi api, by Danilo.')
+})
+
+// server running -----------------------------------
+app.listen(app.get('port'), ()=>{
+    console.log('server running on port', app.get('port'))
+})
