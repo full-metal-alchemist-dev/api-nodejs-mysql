7e771f3f050e3ffebbc1c5c39fe6acfc353fb7fb
alchemist <159976232+full-metal-alchemist-dev@users.noreply.github.com>
2024-07-19 20:58:21 -0600

    metodo get
    metodo get

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require('express')
+const mysql = require('mysql')
+const myconn = require('express-myconnection')
+
+const routes = require('./routes')
 
 const app = express()
 app.set('port', process.env.PORT || 9000)
+const dbOptions = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'library'
+}
+
+// middlewares --------------------------------------
+app.use(myconn(mysql, dbOptions, 'single'))
 
 // routes -------------------------------------------
 app.get('/',(req,res)=>{
     res.send('Bienvenido a mi API, by Danilo.')
 })
+app.use('/api', routes)
 
 // server running -----------------------------------
 app.listen(app.get('port'), ()=>{
     console.log('server running on port', app.get('port'))
 })
+
